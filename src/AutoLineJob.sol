a6ac1727bc9e7a264cbe042df2c25b4130bbe287
Sam MacPherson <xylom44@gmail.com>
2021-11-18 13:27:10 -0500

    add actual rules for AutoLineJOb

--- src/AutoLineJob.sol
+++ src/AutoLineJob.sol
@@ -21,27 +21,37 @@ interface SequencerLike {
     function isMaster(bytes32 network) external view returns (bool);
 }
 
-interface AutoLineLike {
-    function exec(bytes32) external returns (uint256);
-}
-
 interface IlkRegistryLike {
     function list() external view returns (bytes32[] memory);
 }
 
+interface AutoLineLike {
+    function vat() external view returns (address);
+    function ilks(bytes32) external view returns (uint256, uint256, uint48, uint48, uint48);
+    function exec(bytes32) external returns (uint256);
+}
+
+interface VatLike {
+    function ilks(bytes32) external view returns (uint256, uint256, uint256, uint256, uint256);
+}
+
 // Trigger autoline updates based on thresholds
 contract AutoLineJob is IJob {
+
+    uint256 constant internal BPS = 10 ** 4;
     
     SequencerLike public immutable sequencer;
     IlkRegistryLike public immutable ilkRegistry;
     AutoLineLike public immutable autoline;
-    uint256 public immutable tlo;
-    uint256 public immutable thi;
+    VatLike public immutable vat;
+    uint256 public immutable tlo;                       // % below the previously exec'ed debt level
+    uint256 public immutable thi;                       // % above the previously exec'ed debt level
 
     constructor(address _sequencer, address _ilkRegistry, address _autoline, uint256 _tlo, uint256 _thi) {
         sequencer = SequencerLike(_sequencer);
         ilkRegistry = IlkRegistryLike(_ilkRegistry);
         autoline = AutoLineLike(_autoline);
+        vat = VatLike(autoline.vat());
         tlo = _tlo;
         thi = _thi;
     }
@@ -53,7 +63,23 @@ contract AutoLineJob is IJob {
         for (uint256 i = 0; i < ilks.length; i++) {
             bytes32 ilk = ilks[i];
 
-            // TODO evaluate lower/raise DCs
+            (uint256 Art, uint256 rate,, uint256 line,) = vat.ilks(ilk);
+            uint256 debt = Art * rate;
+            (uint256 maxLine, uint256 gap, uint48 ttl, uint48 last, uint48 lastInc) = autoline.ilks(ilk);
+            uint256 nextLine = debt + gap;
+            if (nextLine > maxLine) nextLine = maxLine;
+
+            // Check autoline rules
+            if (maxLine == 0) continue;                                                 // Ilk is not enabled
+            if (last == block.number) continue;                                         // Already triggered this block
+            if (nextLine > line && block.timestamp < lastInc + ttl) continue;           // TTL hasn't expired
+
+            // Check if nextLine is outside our do-nothing range (special exception if nextLine == maxLine)
+            if (nextLine != maxLine && (nextLine < line - gap * tlo / BPS || line + gap * thi / BPS < nextLine)) continue;
+            if (line == maxLine) continue;  // Don't trigger if we are already at maxLine
+
+            // Good to adjust!
+            return (true, address(autoline), abi.encodeWithSelector(AutoLineLike.exec.selector, ilk));
         }
 
         return (false, address(0), bytes("No ilks ready"));
