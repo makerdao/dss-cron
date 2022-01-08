233b62e35a7bd030a5228b1c7d94f64483637c30
Sam MacPherson <xylom44@gmail.com>
2022-01-08 12:35:19 -0500

    add natspec to IJob

--- src/interfaces/IJob.sol
+++ src/interfaces/IJob.sol
@@ -15,7 +15,21 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 pragma solidity >=0.8.0;
 
+/// @title Maker Keeper Network Job
+/// @notice A job represents an independant unit of work that can be done by a keeper
 interface IJob {
+
+    /// @notice Executes this unit of work
+    /// @dev Should revert iff workable() returns canWork of false
+    /// @param network The name of the external keeper network
+    /// @param args Custom arguments supplied to the job, should be copied from workable response
     function work(bytes32 network, bytes calldata args) external;
+
+    /// @notice Ask this job if it has a unit of work available
+    /// @dev This should never revert, only return false if nothing is available
+    /// @param network The name of the external keeper network
+    /// @return canWork Returns true if a unit of work is available
+    /// @return args The custom arguments to be provided to work() or an error string if canWork is false
     function workable(bytes32 network) external returns (bool canWork, bytes memory args);
+
 }
