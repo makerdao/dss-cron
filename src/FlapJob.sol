fe039ab23b30b631be5f4ab113e47f2984137bee
oldchili <130549691+oldchili@users.noreply.github.com>
2023-05-24 21:53:41 +0300

    Add flap job

---
+++ src/FlapJob.sol
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+// Copyright (C) 2021 Dai Foundation
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+pragma solidity 0.8.13;
+
+import {IJob} from "./interfaces/IJob.sol";
+
+interface SequencerLike {
+    function isMaster(bytes32 network) external view returns (bool);
+}
+
+interface VatLike {
+    function sin(address) external view returns (uint256);
+}
+
+interface VowLike {
+    function Sin() external view returns (uint256);
+    function Ash() external view returns (uint256);
+    function heal(uint256) external;
+    function flap() external;
+}
+
+/// @title Call flap when possible
+contract FlapJob is IJob {
+
+    SequencerLike public immutable sequencer;
+    VatLike       public immutable vat;
+    VowLike       public immutable vow;
+
+    // --- Errors ---
+    error NotMaster(bytes32 network);
+
+    // --- Events ---
+    event Work(bytes32 indexed network);
+
+    constructor(address _sequencer, address _vat, address _vow) {
+        sequencer = SequencerLike(_sequencer);
+        vat       = VatLike(_vat);
+        vow       = VowLike(_vow);
+    }
+
+    function work(bytes32 network, bytes calldata args) public {
+        if (!sequencer.isMaster(network)) revert NotMaster(network);
+
+        uint256 toHeal = abi.decode(args, (uint256));
+        if (toHeal > 0) vow.heal(toHeal);
+        vow.flap();
+
+        emit Work(network);
+    }
+
+    function workable(bytes32 network) external override returns (bool, bytes memory) {
+        if (!sequencer.isMaster(network)) return (false, bytes("Network is not master"));
+
+        bytes memory args;
+        uint256 unbackedTotal = vat.sin(address(vow));
+        uint256 unbackedVow   = vow.Sin() + vow.Ash();
+
+        // Check if need to cancel out free unbacked debt with system surplus
+        uint256 toHeal = unbackedTotal > unbackedVow ? unbackedTotal - unbackedVow : 0;
+        args = abi.encode(toHeal);
+
+        try this.work(network, args) {
+            // Flap succeeds
+            return (true, args);
+        } catch {
+            // Can not flap -- carry on
+        }
+        return (false, bytes("Flap not possible"));
+    }
+}
---
+++ src/tests/FlapJob-integration.t.sol
@@ -0,0 +1,95 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+// Copyright (C) 2021 Dai Foundation
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+pragma solidity 0.8.13;
+
+import "./DssCronBase.t.sol";
+
+import {FlapJob} from "../FlapJob.sol";
+
+contract FlapJobIntegrationTest is DssCronBaseTest {
+    using stdStorage for StdStorage;
+    using GodMode for *;
+
+    FlapJob flapJob;
+
+    event LogNote(
+        bytes4   indexed  sig,
+        address  indexed  usr,
+        bytes32  indexed  arg1,
+        bytes32  indexed  arg2,
+        bytes             data
+    ) anonymous;
+
+    function setUpSub() virtual override internal {
+        flapJob = new FlapJob(address(sequencer), address(dss.vat), address(dss.vow));
+
+        // Make sure that if a flapper has a cooldown period it already passed
+        GodMode.vm().warp(block.timestamp + 10 days);
+
+        // Set default values that assure flap will succeed without a need to heal
+        stdstore.target(address(dss.vat)).sig("dai(address)").with_key(address(dss.vow)).depth(0).checked_write(70 * MILLION * RAD);
+        stdstore.target(address(dss.vat)).sig("sin(address)").with_key(address(dss.vow)).depth(0).checked_write(uint256(0));
+        stdstore.target(address(dss.vow)).sig("hump()").checked_write(50 * MILLION * RAD);
+        stdstore.target(address(dss.vow)).sig("bump()").checked_write(10 * THOUSAND * RAD);
+        stdstore.target(address(dss.vow)).sig("Sin()").checked_write(uint256(0));
+        stdstore.target(address(dss.vow)).sig("Ash()").checked_write(uint256(0));
+
+    }
+
+    function test_flap_no_need_to_heal() public {
+        (bool canWork, bytes memory args) = flapJob.workable(NET_A);
+        assertTrue(canWork, "Should be able to work");
+
+        vm.expectEmit(false, false, false, false);
+        emit LogNote(dss.vow.flap.selector, address(0), 0, 0, bytes(""));
+        flapJob.work(NET_A, args);
+    }
+
+    function test_flap_need_to_heal() public {
+        // force free bad debt of 1
+        uint256 newVatSin = dss.vow.Sin() + dss.vow.Ash() + 1;
+        stdstore.target(address(dss.vat)).sig("sin(address)").with_key(address(dss.vow)).depth(0).checked_write(newVatSin);
+
+        uint256 snapshot = vm.snapshot();
+        (bool canWork, bytes memory args) = flapJob.workable(NET_A);
+        assertTrue(canWork, "Should be able to work");
+        vm.revertTo(snapshot);
+
+        vm.expectEmit(false, false, false, false);
+        emit LogNote(dss.vow.heal.selector, address(0), 0, 0, bytes(""));
+        vm.expectEmit(false, false, false, false);
+        emit LogNote(dss.vow.flap.selector, address(0), 0, 0, bytes(""));
+        flapJob.work(NET_A, args);
+    }
+
+    function test_flap_heal_fails() public {
+        // force system surplus to be negative
+        uint256 newVatSin = dss.vat.dai(address(dss.vow)) + 1;
+        stdstore.target(address(dss.vat)).sig("sin(address)").with_key(address(dss.vow)).depth(0).checked_write(newVatSin);
+
+        (bool canWork,) = flapJob.workable(NET_A);
+        assertTrue(!canWork, "Should not be able to work");
+    }
+
+    function test_flap_fails() public {
+        // force hump to be higher than the SB
+        uint256 newVowHump = dss.vat.dai(address(dss.vow)) + 1;
+        stdstore.target(address(dss.vow)).sig("hump()").checked_write(newVowHump);
+
+        (bool canWork,) = flapJob.workable(NET_A);
+        assertTrue(!canWork, "Should not be able to work");
+    }
+}
