c274a589cdc186db751f9c47b65ae40678e345fd
0xp3th1um <0xp3th1um@dewiz.xyz>
2024-05-08 13:54:18 +0300

    refactor: revert changes in  LitePsmJob

--- src/LitePsmJob.sol
+++ src/LitePsmJob.sol
@@ -30,7 +30,7 @@ interface LitePsmLike {
     function trim() external returns (uint256 wad);
 }
 
-/// @title Call fill/chug/trim when possible
+/// @title Call flap when possible
 contract LitePsmJob is IJob {
     SequencerLike public immutable sequencer;
     LitePsmLike public immutable litePsm;
